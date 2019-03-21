5edca8b101ff4cb449c7718757ed509d4a6d201e
lin_work_win <892888763@qq.com>
2019-03-21 16:47:16 +0800

    feat(change project):实现Linux环境文件拷贝

--- test_linux_pro/simple_linux_pro/main.cpp
+++ test_linux_pro/simple_linux_pro/main.cpp
@@ -1,11 +1,54 @@
 #include <iostream>
+#include <string>
 
-void test_function() {
-  std::cout << "test visual studio linux program." << std::endl;
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define MAXSIZE 256
+
+void copy_file(std::string const& src_filename, std::string const& dsc_filename) {
+  int open_fd_src = -1, open_fd_dsc = -1;
+  int call_api_ret = -1;
+  int read_bytes = -1;
+
+  // 打开指定文件
+  open_fd_src = open(src_filename.c_str(), O_RDONLY);
+  if (open_fd_src == -1) {
+    std::cout << "open " << src_filename << " file failed." << std::endl;
+    return;
+  }
+
+  int openFlags, filePerms;
+  openFlags = O_CREAT | O_WRONLY | O_TRUNC;
+  filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP |
+              S_IROTH | S_IWOTH;      /* rw-rw-rw- */
+  open_fd_dsc = open(dsc_filename.c_str(), openFlags, filePerms);
+  if (open_fd_dsc == -1) {
+    std::cout << "open " << open_fd_dsc << " file failed." << std::endl;
+    return;
+  }
+
+  // 读取文件
+  char buf[MAXSIZE];
+  read_bytes = read(open_fd_src, buf, MAXSIZE);
+  if (read_bytes == -1) {
+    std::cout << "read " << src_filename << " file failed." << std::endl;
+    return;
+  }
+
+  // 写入文件
+  call_api_ret = write(open_fd_dsc, buf, read_bytes);
+  if (call_api_ret == -1) {
+    std::cout << "read " << src_filename << " file failed." << std::endl;
+    return;
+  }
 }
 
 int main() {
-  test_function();
+  std::string source_file_name("/root/projects/simple_linux_pro/file");
+  std::string desc_file_name("/root/projects/simple_linux_pro/file1");
+  copy_file(source_file_name, desc_file_name);
 
   return 0;
 }
