06a31e5d84354da7a4ac27946432441d7ec1a502
lin_work_win <892888763@qq.com>
2019-03-23 12:40:35 +0800

    feat(network.copy file):使用网络分包传输文件例子(数据传输还有问题，对端没有收到)

---
+++ test_linux_pro/network_client/client_transfer_tcp_data.cpp
@@ -0,0 +1,84 @@
+#include <unistd.h>
+#include <arpa/inet.h>  /* inet(3) functions */
+#include <netdb.h>      /*gethostbyname function */
+#include <netinet/in.h> /* sockaddr_in{} and other Internet defns */
+#include <sys/socket.h> /* basic socket definitions */
+#include <sys/types.h>  /* basic system data types */
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <iostream>
+
+//------------------- function define ----------------------------------
+void trans_file_by_socket(int sockfd);
+
+int main() {
+  char *servInetAddr = (char *)malloc(10);
+  sprintf(servInetAddr, "%s", "127.0.0.1");
+  int servPort = 6888;
+  int connfd;
+  struct sockaddr_in servaddr;
+
+  // 写死ip和port
+  servPort = 6888;
+  strcpy(servInetAddr, "127.0.0.1");
+
+  connfd = socket(AF_INET, SOCK_STREAM, 0);
+
+  bzero(&servaddr, sizeof(servaddr));
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_port = htons(servPort);
+  inet_pton(AF_INET, servInetAddr, &servaddr.sin_addr);
+
+  int connect_server_ret;
+  do
+  {
+    sleep(1);
+    connect_server_ret = connect(connfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+    printf("connect_server_ret: %d\n", connect_server_ret);
+  } while (connect_server_ret < 0);
+
+  trans_file_by_socket(connfd); /* do it all */
+  close(connfd);
+  printf("exit\n");
+
+  return 0;
+}
+
+// 解析本地文件，然后使用网络模块发送到服务端
+#define MAXSIZE 256
+void trans_file_by_socket(int sockfd) {
+  std::string source_file_name("./file");
+
+  // 打开指定文件
+  int open_fd_src = open(source_file_name.c_str(), O_RDONLY);
+  if (open_fd_src == -1) {
+    std::cout << "open " << source_file_name << " file failed, errno: "<< errno << std::endl;
+    return;
+  }
+
+  // 分块解析打开的文件
+  char buf[MAXSIZE];
+  size_t read_bytes = -1;
+  size_t api_ret;
+  while ((read_bytes = read(open_fd_src, buf, MAXSIZE)) > 0) {
+    // 使用网络接口传输读取到的数据
+    api_ret = write(sockfd, buf, strlen(buf));
+    if (api_ret == -1) {
+      std::cout << "write data to socket meet err." << std::endl;
+      continue;
+    }
+
+    ///TODO 等待对端应答
+    //api_ret = read(sockfd, recvline, MAXLINE);
+  }
+  
+  sleep(100);
+}
---
+++ test_linux_pro/network_service/service_handle_tcp_data.cpp
@@ -0,0 +1,117 @@
+#include <unistd.h>
+#include <arpa/inet.h>  /* inet(3) functions */
+#include <netinet/in.h> /* sockaddr_in{} and other Internet defns */
+#include <sys/socket.h> /* basic socket definitions */
+#include <sys/types.h>  /* basic system data types */
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <iostream>
+
+#define MAXLINE 1024
+void handle_network_data_by_socket(int connfd);
+
+int main() {
+  int listenfd, connfd;
+  int serverPort = 6888;
+  int listenq = 1024;
+  pid_t child_pid;
+  socklen_t socklen;
+
+  struct sockaddr_in cliaddr, servaddr;
+  socklen = sizeof(cliaddr);
+
+  bzero(&servaddr, sizeof(servaddr));
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  servaddr.sin_port = htons(serverPort);
+
+  listenfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (listenfd < 0) {
+    perror("socket error");
+    return -1;
+  }
+
+  // 处理 address already in use. 错误
+  int enable = 1;
+  if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) <
+    0) {
+    perror("setsockopt(SO_REUSEADDR) failed");
+    return -1;
+  }
+
+  if (bind(listenfd, (struct sockaddr *)&servaddr, socklen) < 0) {
+    perror("bind error");
+    return -1;
+  }
+  if (listen(listenfd, listenq) < 0) {
+    perror("listen error");
+    return -1;
+  }
+  printf("echo server startup,listen on port:%d\n", serverPort);
+  for (;;) {
+    connfd = accept(listenfd, (struct sockaddr *)&cliaddr, &socklen);
+    if (connfd < 0) {
+      perror("accept error");
+      continue;
+    }
+
+    printf("accept form %s:%d\n", inet_ntoa(cliaddr.sin_addr),
+      cliaddr.sin_port);
+
+    child_pid = fork();
+    if (child_pid == 0) { //child process
+      //close(listenfd); // close listening socket ??? 为啥要关闭掉
+      handle_network_data_by_socket(connfd);
+      exit(0);
+    }
+    else if (child_pid > 0) {
+      // parent process
+      //close(connfd); // closes connected socket ??? 为啥要关闭掉
+    }
+    else {
+      perror("fork error");
+    }
+  }
+
+  return 0;
+}
+
+// 解析本地文件，然后使用网络模块发送到服务端
+#define MAXSIZE 256
+void handle_network_data_by_socket(int sockfd) {
+  std::string source_file_name("./file1");
+
+  // 打开指定文件
+  int openFlags, filePerms;
+  openFlags = O_CREAT | O_WRONLY /*| O_TRUNC*/;
+  filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP |
+              S_IROTH | S_IWOTH;      /* rw-rw-rw- */
+  int open_fd_dsc = open(source_file_name.c_str(), openFlags, filePerms);
+  if (open_fd_dsc == -1) {
+    std::cout << "open " << source_file_name << " file failed, errno: " << errno << std::endl;
+    return;
+  }
+
+  // 分块接收发过来的数据，并写入本地文件
+  char buf[MAXSIZE];
+  size_t read_bytes = -1;
+  size_t api_ret;
+  std::cout << "begin receive data."<< std::endl;
+  while ((read_bytes = read(sockfd, buf, MAXSIZE)) > 0) {
+    std::cout << "receive data:" << buf << std::endl;
+    // 使用网络接口传输读取到的数据
+    api_ret = write(open_fd_dsc, buf, strlen(buf));
+    if (api_ret == -1) {
+      std::cout << "write data to socket meet err." << std::endl;
+      continue;
+    }
+  }
+}
